337d8f466b22ea8c4e5324cd83b28dbbe8c63267
Divya Kaushik <kaushikdivya16@gmail.com>
2015-10-31 13:32:09 -0700

    saving work

--- routes.py
+++ routes.py
@@ -22,6 +22,11 @@ app.config.update({
 })
 
 # app.secret_key = 'number_guessing_game'
+#need to check what this cache control is and it's values.
+def no_cache(response):
+    response.headers.add('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0')
+    response.headers.add('Pragma', 'no-cache')
+    return response
 
 def connect_db():
     return msql.connect(
@@ -58,7 +63,7 @@ def login():
             session['access_token']= user_info['access_token']
             print "this is sesssion info: %s" %(dir(session))
             response = make_response(redirect(url_for('users')))
-            return response
+            return no_cache(response)
 
 
     else:
@@ -87,73 +92,35 @@ def home():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-DUMMY_USERS = {
-    'meta': {},
-    'data': {
-        'users': [
-            {
-                'name': 'Divya',
-                'email': 'email',
-                'access_token': 'abc1234'
-            }
-        ],
-        'lists': [
-            {
-                'id': 1,
-                'name': 'grocery list',
-                'items': [
-                    {
-                        'name': 'tide',
-                        'status': 'incomplete'
-                    },
-                    {
-                        'name': 'wine',
-                        'status': 'incomplete'
-                    }
-                ]
-            },
-            {
-                'id': 2,
-                'name': 'hawaii trip planning',
-                'items': [
-                    {
-                        'name': 'Beer',
-                        'status': 'incomplete'
-                    },
-                    {
-                        'name': 'air tickets',
-                        'status': 'incomplete'
-                    }
-                ]
-            }
-        ]
-    }
-}
-
 
 @app.route('/users/', methods=['GET'])
 def users():
-    access_token = session.get('access_token', None)
-    db = connect_db ()
-    try:
-        user_info = auth.authenticate_using_access_token(access_token, db)
-        user_id = user_info['id']
-        list_of_lists = dm.list_of_list(user_id, db)
-    except AccessTokenExpiredError:
-        flash("Access Token Expired. Login again")
-        return render_template(url_for('login'))
-    except InvalidAccessTokenError:
-        flash("In Valid Access Token. Login again")
-        return render_template(url_for('login'))
-    except NoListFoundError:
-        flash("No List Created yet.")
-        return render_template(url_for('users', user_id = user_id )) #need to check this. as it seems it go in infinite iteration when no list is added by user.
+    if 'access_token' in session:
+        access_token = session.get('access_token', None)
+        db = connect_db ()
+        try:
+            user_info = auth.authenticate_using_access_token(access_token, db)
+            user_id = user_info['id']
+            list_of_lists = dm.list_of_list(user_id, db)
+        except AccessTokenExpiredError:
+            flash("Access Token Expired. Login again")
+            return render_template(url_for('login'))
+        except InvalidAccessTokenError:
+            flash("In Valid Access Token. Login again")
+            return render_template(url_for('login'))
+        except NoListFoundError:
+            flash("No List Created yet.")
+            return render_template(url_for('users', user_id = user_id )) #need to check this. as it seems it go in infinite iteration when no list is added by user.
 
 
-    print type(user_info)
-    if request.method == 'GET':
-        print  user_info
-        return render_template('user_home.html', user_info = user_info, lists = list_of_lists, access_token = access_token)
+        print type(user_info)
+        if request.method == 'GET':
+            print  user_info
+            response = make_response(render_template('user_home.html', user_info = user_info, lists = list_of_lists, access_token = access_token))
+            return no_cache(response)
+    else:
+
+        return redirect(url_for('login'))
     # else:
     #     dm.deactive_user(user_info['id'], query_access_token, db)
     #     response_data = {
@@ -180,44 +147,78 @@ def users():
     #  return render_template('user_home.html', user_data=DUMMY_USERS)
 
 
-def get_list_data(list_id, access_token):
-    try:
-        return  [
-                    DUMMY_USERS['data']['lists'][list_id-1]
-                ]
-    except (KeyError, IndexError):
-        return {
-            'data': {}
-        }
 
-@app.route('/lists', methods=['GET'])
-def user_home():
-    pass
+@app.route('/lists/', methods=["POST"])
+def new_list():
+    #import pdb; pdb.set_trace()
+    list_name = request.form['list_name']
+    if list_name is not None and len(list_name) == 0:
+        list_name = None
+    if 'access_token' in session:
+        access_token = session.get('access_token', None)
+        db = connect_db ()
+        try:
+            user_info = auth.authenticate_using_access_token(access_token, db)
+            user_id = user_info['id']
+        except AccessTokenExpiredError:
+            flash("Access Token Expired. Login again")
+            return redirect(url_for('login'))
+        except InvalidAccessTokenError:
+            flash("In Valid Access Token. Login again")
+            return redirect(url_for('login'))
+        if list_name == None:
+            flash('Enter the name of the new list')
+            return redirect(url_for('users'))
+        else:
+            dm.create_new_list(list_name, user_id, db)
+            return redirect(url_for('users'))
+    else:
+        return redirect(url_for('login'))
+
+
 @app.route('/lists/<int:list_id>', methods=['GET'])
 def lists(list_id):
-    print 'list_id: %s, access_token: %s' % (list_id, request.args.get('access_token'))
-    access_token = session.get('access_token', None)
-    db = connect_db ()
-    try:
-        user_info = auth.authenticate_using_access_token(access_token, db)
-        user_id = user_info['id']
-        list_info = dm.fetch_list(list_id, user_id, db)
-    except AccessTokenExpiredError:
-        flash("Access Token Expired. Login again")
-        return render_template(url_for('login'))
-    except InvalidAccessTokenError:
-        flash("In Valid Access Token. Login again")
-        return render_template(url_for('login'))
-    except NoListFoundError:
-        flash("No List found with this id:%d" %(list_id))
-        return render_template(url_for('users', user_id = user_info['id'],
-        access_token = user_info['access_token'] ))
-
-    return render_template('list_info.html', list_info=list_info)
+    if 'access_token' in session:
+        print 'list_id: %s, access_token: %s' % (list_id, request.args.get('access_token'))
+        access_token = session.get('access_token', None)
+        db = connect_db ()
+        try:
+            user_info = auth.authenticate_using_access_token(access_token, db)
+            user_id = user_info['id']
+            list_info = dm.fetch_list(list_id, user_id, db)
+        except AccessTokenExpiredError:
+            flash("Access Token Expired. Login again")
+            return redirect(url_for('login'))
+        except InvalidAccessTokenError:
+            flash("In Valid Access Token. Login again")
+            return redirect(url_for('login'))
+        except NoListFoundError:
+            flash("No List found with this id:%d" %(list_id))
+            return redirect(url_for('users', user_id = user_info['id'],
+            access_token = user_info['access_token'] ))
+        response = make_response(render_template('list_info.html', list_info=list_info))
+        return no_cache(response)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
     # import pdb; pdb.set_trace()
+    if 'access_token' in session:
+        access_token = session.get('access_token', None)
+        db = connect_db ()
+        try:
+            user_info = auth.authenticate_using_access_token(access_token, db)
+            user_id = user_info['id']
+        except AccessTokenExpiredError:
+            flash("Access Token Expired. Login again")
+            return redirect(url_for('login'))
+        except InvalidAccessTokenError:
+            flash("In Valid Access Token. Login again")
+            return redirect(url_for('login'))
+        dm.logout_user(access_token, db)
+        session.pop('access_token', None)
+        return redirect(url_for('home'))
 
-    session.pop('access_token', None)
-    return redirect(url_for('home'))
+    else:
+        return redirect(url_for('login'))
